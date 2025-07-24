1e1903142ce2c256e108b6320c71eb7461ebdef4
lin-dongyizhi7 <2985956026@qq.com>
2025-07-24 13:50:57 +0800

    wip: 新增test_qgis版

---
+++ communicators/test_qgis.py
@@ -0,0 +1,19 @@
+from operation import Operation
+
+if __name__ == "__main__":
+    # 1. 实例化操作类
+    op = Operation(test_machine_ip="192.168.5.129", test_machine_port=8888)
+
+    op.click_element(element_path="Project Toolbar/New", role_name="push button")
+
+    op.click_element(element_path="Web", role_name="menu item")
+
+    op.click_element(element_path="QuickMapServices", role_name="menu item")
+
+    op.click_element(element_path="OSM", role_name="menu item")
+
+    op.click_element(element_path="OSM Standard", role_name="menu item")
+
+    op.drag_map_percentage(0.6, 0.7, 0.5, 0.6)
+
+    op.click_element(element_path="Map Navigation Toolbar/Zoom to Layer(s)", role_name="push button")
