f6562342cde76a1a14aff6c54daef02eecf852d0
lin-dongyizhi7 <2985956026@qq.com>
2025-07-25 14:52:58 +0800

    update:计算器测试特殊字符按钮替换

--- communicators/test_calculator.py
+++ communicators/test_calculator.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
 
     # 计算 5 * 6 - 7 的过程
     op.click_element(element_path="5", role_name_list=["push button"])  # 点击数字5
-    op.click_element(element_path="*", role_name_list=["push button"])  # 点击乘号
+    op.click_element(element_path="\303\227", role_name_list=["push button"])  # 点击乘号
     op.click_element(element_path="6", role_name_list=["push button"])  # 点击数字6
     op.click_element(element_path="-", role_name_list=["push button"])  # 点击减号
     op.click_element(element_path="7", role_name_list=["push button"])  # 点击数字7
@@ -24,6 +24,39 @@ if __name__ == "__main__":
     # 计算 81 / 9 的过程
     op.click_element(element_path="8", role_name_list=["push button"])  # 点击数字8
     op.click_element(element_path="1", role_name_list=["push button"])  # 点击数字1
-    op.click_element(element_path="/", role_name_list=["push button"])  # 点击除号
+    op.click_element(element_path="\303\267", role_name_list=["push button"])  # 点击除号
     op.click_element(element_path="9", role_name_list=["push button"])  # 点击数字9
+    op.click_element(element_path="=", role_name_list=["push button"])  # 点击等号
+
+    # 计算 6.5 + 8.5 - 3.4 的过程
+    op.click_element(element_path="6", role_name_list=["push button"])  # 点击数字6
+    op.click_element(element_path=".", role_name_list=["push button"])  # 点击小数点
+    op.click_element(element_path="5", role_name_list=["push button"])  # 点击数字5
+    op.click_element(element_path="+", role_name_list=["push button"])  # 点击加号
+    op.click_element(element_path="8", role_name_list=["push button"])  # 点击数字8
+    op.click_element(element_path=".", role_name_list=["push button"])  # 点击小数点
+    op.click_element(element_path="5", role_name_list=["push button"])  # 点击数字5
+    op.click_element(element_path="-", role_name_list=["push button"])  # 点击减号
+    op.click_element(element_path="3", role_name_list=["push button"])  # 点击数字3
+    op.click_element(element_path=".", role_name_list=["push button"])  # 点击小数点
+    op.click_element(element_path="4", role_name_list=["push button"])  # 点击数字4
+    op.click_element(element_path="=", role_name_list=["push button"])  # 点击等号
+
+    # 计算 2.5 * 4.2 / 1.2 的过程
+    op.click_element(element_path="2", role_name_list=["push button"])  # 点击数字2
+    op.click_element(element_path=".", role_name_list=["push button"])  # 点击小数点
+    op.click_element(element_path="5", role_name_list=["push button"])  # 点击数字5
+    op.click_element(element_path="\303\227", role_name_list=["push button"])  # 点击乘号
+    op.click_element(element_path="4", role_name_list=["push button"])  # 点击数字4
+    op.click_element(element_path=".", role_name_list=["push button"])  # 点击小数点
+    op.click_element(element_path="2", role_name_list=["push button"])  # 点击数字2
+    op.click_element(element_path="\303\267", role_name_list=["push button"])  # 点击除号
+    op.click_element(element_path="1", role_name_list=["push button"])  # 点击数字1
+    op.click_element(element_path=".", role_name_list=["push button"])  # 点击小数点
+    op.click_element(element_path="2", role_name_list=["push button"])  # 点击数字2
+    op.click_element(element_path="=", role_name_list=["push button"])  # 点击等号
+
+    # 计算 5² 的过程
+    op.click_element(element_path="5", role_name_list=["push button"])  # 点击数字5
+    op.click_element(element_path="\303\262", role_name_list=["push button"])  # 点击平方按钮
     op.click_element(element_path="=", role_name_list=["push button"])  # 点击等号
